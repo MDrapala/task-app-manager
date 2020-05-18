46c8f2a8f02ad5b90cef212081171851fd3d082a
MDrapala <drapalamathieu95@gmail.com>
2020-05-18 08:46:14 +0200

    End Resource Updating Endpoints Part II

--- src/index.js
+++ src/index.js
@@ -46,6 +46,30 @@ app.get('/users/:id', async (req, res) => {
     }
 })
 
+//updates
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (isValidOperation) {
+        return res.status(400).send({ error: 'Invalide updates!' })
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })  
+        
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+
 //Add tasks
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
@@ -57,6 +81,7 @@ app.post('/tasks', async (req, res) => {
         res.status(400).send(e)
     }
 })
+
 
 //get  all tasks
 app.get('/tasks', async (req, res) => {
@@ -83,6 +108,29 @@ app.get('/tasks/:id', async (req, res) => {
         res.status(500).send()
     }
 })
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if(!isValidOperation) {
+        return res.status(400).send({error: 'Invalidate Updates!'})
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
+
+        if(!task) {
+            return res.status(404).send
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
 
 app.listen(port, () => {
     console.log('Serveur is up on port '+ port) 
